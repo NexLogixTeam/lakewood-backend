9daa18bec18209355eff1f601c6e13864be8c7ac
Ali Waqar <aliwaqar981@gmail.com>
2025-08-13 18:35:17 +0500

    fixed typesense

--- functions/index.js
+++ functions/index.js
@@ -9,33 +9,32 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const logger = require('firebase-functions/logger');
 
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 let db = admin.firestore();
 db.settings({ timestampsInSnapshots: true });
 
-const Typesense = require("typesense");
+const Typesense = require('typesense');
 
 const client = new Typesense.Client({
   nodes: [
     {
-      host: "n2zuwmjsilropd1xp-1.a1.typesense.net", // Replace with your Typesense server host
+      host: 'tb3j850e7w6yi1dxp-1.a1.typesense.net', // Replace with your Typesense server host
       port: 443, // Replace with your server port
-      protocol: "https", // Use 'https' if using Typesense Cloud or a secured server
+      protocol: 'https', // Use 'https' if using Typesense Cloud or a secured server
     },
   ],
-  apiKey: "7J6yMPeyWPSbPbR4q9tNtp3cgwi1fbc8", // Replace with your API key
+  apiKey: '8JocHBdIQzmxfFNCkJU8TmLgX6Wp8HDA', // Replace with your API key
   connectionTimeoutSeconds: 4,
 });
 
 // Firestore trigger for new product addition
 exports.syncNewProductToTypesense = functions.firestore
-  .document("Products/{productId}")
+  .document('Products/{productId}')
   .onCreate(async (snap, context) => {
     const newProduct = snap.data();
     const productId = context.params.productId;
@@ -43,16 +42,16 @@ exports.syncNewProductToTypesense = functions.firestore
     try {
       // Add the new product to Typesense
       await client
-        .collections("Products")
+        .collections('Products')
         .documents()
         .create({
           ...newProduct,
           id: productId,
-          createdAt: newProduct.createdAt ? newProduct.createdAt.toDate() : "",
+          createdAt: newProduct.createdAt ? newProduct.createdAt.toDate() : '',
         });
 
       await client
-        .collections("Products")
+        .collections('Products')
         .documents(updatedBook.id)
         .update(updatedBook);
       logger.log(`Successfully added product ${productId} to Typesense`);
@@ -63,7 +62,7 @@ exports.syncNewProductToTypesense = functions.firestore
 
 // Firestore trigger for new product addition
 exports.syncUpdatedProductToTypesense = functions.firestore
-  .document("Products/{productId}")
+  .document('Products/{productId}')
   .onUpdate(async (snap, context) => {
     const updatedProduct = snap.data();
     const productId = context.params.productId;
@@ -71,14 +70,14 @@ exports.syncUpdatedProductToTypesense = functions.firestore
     try {
       // Add the new product to Typesense
       await client
-        .collections("Products")
+        .collections('Products')
         .documents(productId)
         .update({
           ...updatedProduct,
           id: productId,
           createdAt: updatedProduct.createdAt
             ? updatedProduct.createdAt.toDate()
-            : "",
+            : '',
         });
 
       logger.log(`Successfully product updated ${productId} to Typesense`);
@@ -88,27 +87,27 @@ exports.syncUpdatedProductToTypesense = functions.firestore
   });
 
 async function getUser(userId) {
-  let userRef = db.collection("Users").doc(userId);
+  let userRef = db.collection('Users').doc(userId);
   let getDoc = userRef
     .get()
     .then((doc) => {
       if (!doc.exists) {
-        console.log("No such document!");
+        console.log('No such document!');
         return 0;
       } else {
-        console.log("Document data:", doc.data());
+        console.log('Document data:', doc.data());
         return doc.data();
       }
     })
     .catch((err) => {
-      console.log("Error getting document", err);
+      console.log('Error getting document', err);
     });
   // [END get_document]
   return getDoc;
 }
 
 exports.sendNotification = functions.firestore
-  .document("Notifications/{id}")
+  .document('Notifications/{id}')
   .onCreate(async (snapshot, context) => {
     const user = await getUser(snapshot.data().userId);
     let recipientDetails;
@@ -116,7 +115,7 @@ exports.sendNotification = functions.firestore
       recipientDetails = {
         recipientId: snapshot.data().recipientId,
         productId: snapshot.data().productId,
-        screen: "ChatDetails",
+        screen: 'ChatDetails',
         badge: user.unreadNotifications || 0,
       };
     } else {
@@ -137,23 +136,23 @@ exports.sendNotification = functions.firestore
       },
       apns: {
         headers: {
-          "apns-priority": "10",
+          'apns-priority': '10',
         },
         payload: {
           aps: {
-            sound: "horn.wav",
+            sound: 'horn.wav',
             badge: user.unreadNotifications || 0,
           },
         },
       },
       ios: {
-        sound: "horn.wav",
+        sound: 'horn.wav',
       },
       android: {
-        priority: "high",
+        priority: 'high',
         notification: {
-          channel_id: "sound_channel",
-          sound: "horn.wav",
+          channel_id: 'sound_channel',
+          sound: 'horn.wav',
           notification_count: 20,
         },
       },
@@ -165,7 +164,7 @@ exports.sendNotification = functions.firestore
       .then((response) => {
         return logger.info(`A new notification for  =====`, response);
       })
-      .catch((e) => logger.info("------ error:", e));
+      .catch((e) => logger.info('------ error:', e));
   });
 
 /* eslint-enable */
